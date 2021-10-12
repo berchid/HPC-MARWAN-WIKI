596bfe220b764a2f684d446cd7a3426dd8fba6ee
berchid <84917412+berchid@users.noreply.github.com>
2021-10-12 12:07:51 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -59,8 +59,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 
 html_static_path = ['_static']
-
-html_logo = "_static/logo_HPC-MARWAN_1.png"
+html_logo = '_static/logo_HPC-MARWAN_1.png'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
